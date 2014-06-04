b79de4e335717e55d02cdcd6dfc252e6ca2efa20
Robert Leon <llexi@hotmail.com>
2014-06-04 20:02:06 +0100

    added title screen

--- game.rb
+++ game.rb
@@ -11,6 +11,7 @@ class GameWindow < Gosu::Window
 
     @width, @height = width, height
     super(width, height, false)
+    @game_in_progress = false
     self.caption = "The Game of Life"
     @dx, @dy = 10, 10
     @cols, @rows = width / dx, height / dy
@@ -21,7 +22,24 @@ class GameWindow < Gosu::Window
 
     @game = Game.new(World.new(@rows, @cols))
     reset_game
+    title_screen
+    soundtrack
   end
+
+  def title_screen
+  end
+
+  def soundtrack # Game Soundtrack
+    @soundtrack = [] 
+    @soundtrack << Gosu::Song.new("assets/audio/adarkerheart.mp3")
+    @song = @soundtrack.first
+    @song.play(looping = true)
+  end
+
+  def setup_game
+    @game_in_progress = true
+  end
+
 
   def reset_game
     @gen = 0 
@@ -31,11 +49,27 @@ class GameWindow < Gosu::Window
   end
  
   def update
+    if button_down? Gosu::KbQ
+      close
+    end
+    if button_down? Gosu::KbP
+      setup_game unless @game_in_progress
+    end
+    if button_down? Gosu::KbM
+      title_screen unless @game_in_progress == false
+      @game_in_progress = false
+    end
     game.tick
     @gen += 1
   end
 
   def draw
+    unless @game_in_progress
+        text.draw("The Game Of Life", 150, 150, 50, 2.0, 2.0, Gosu::Color.new(0xff30f2e1))
+        text.draw("press 'p' to play", 215, 220, 50, 1, 1, Gosu::Color.new(0xff8030f2))
+        text.draw("press 'q' to quit", 215, 245, 50, 1, 1, Gosu::Color.new(0xff8030f2))
+    end
+    return unless @game_in_progress
     game.world.cells.each do |cell|
       col, row = cell.y, cell.x
       color = cell.alive? ? @alivecol : @deadcol
@@ -54,7 +88,7 @@ class GameWindow < Gosu::Window
     text.draw("Gen: #{@gen}", 50, 0, 50, 1, 1)
     text.draw("Live cells: #{live_cells}", 275, 0, 50, 1, 1)
     text.draw("Pitch: #{speed}", 620, 0, 50, 1, 1)
-    tone.play(10, speed) 
+    tone.play(5, speed) 
   end
 
   def button_down(id)
